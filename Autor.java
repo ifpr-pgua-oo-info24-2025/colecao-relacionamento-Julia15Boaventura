ce1358f21a0b5f03b0025a120d5e90605d6fd910
Julia15Boaventura <juliapowala13@gmail.com>
2025-05-30 18:40:22 -0300

    Autor.java

---
+++ Autor.java
@@ -0,0 +1,21 @@
+public class Autor {
+    private String nome;
+    private Nacionalidade nacionalidade;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Nacionalidade getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(Nacionalidade nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+}
